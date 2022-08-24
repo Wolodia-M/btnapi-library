9eddaa0f3b9c1814477925c1a63d13dedefa779b
Wolodia-M <104127709+Wolodia-M@users.noreply.github.com>
2022-08-24 12:29:43 +0300

    Update btnapi.cpp

--- btnapi.cpp
+++ btnapi.cpp
@@ -1,3 +1,17 @@
+// btnapi library - library for connecting buttons to Arduino
+// Copyright (C) 2022  WolodiaM
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
 #include "btnapi.h"
 /****************************init************************************/
 btnapi::btnapi(int pin, int pull, int open) {
